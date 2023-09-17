96c1a1788253f2e2e6e3cf80755ebd3ad9372260
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2023-09-17 20:43:11 +0530

    Creating html tags without JSX in react

---
+++ app.js
@@ -0,0 +1,17 @@
+const heading = React.createElement("h1", {}, "Hello World form React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am H1 tag from child 2"),
+    React.createElement("h2", {}, "I am H2 tag from child 2"),
+  ]),
+]);
+
+root.render(parent);
