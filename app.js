e4df681e9912118dc51b00243cd50a7effdec65f
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2023-09-24 18:51:52 +0530

    JSX understanding

--- app.js
+++ app.js
@@ -6,15 +6,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am H1 tag"),
-    React.createElement("h2", {}, "I am H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am H1 tag from child 2"),
-    React.createElement("h2", {}, "I am H2 tag from child 2"),
-  ]),
-]);
+const jsxHeading = <h1>Hello World from JSX!</h1>;
 
-root.render(parent);
+root.render(jsxHeading);
